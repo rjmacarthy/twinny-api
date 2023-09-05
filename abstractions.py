724e08f2a5e0089335c262aec3c085073b73cea3
allen-li1231 <supermrli@hotmail.com>
2023-09-05 16:18:03 +0800

    sync api Payload with additional useful settings in twinny extension

--- abstractions.py
+++ abstractions.py
@@ -11,10 +11,12 @@ class Choice(BaseModel):
 
 
 class Payload(BaseModel):
+    max_time: float = None
     max_tokens: int = 100
     num_return_sequences: int
     one_line: bool = True
     prompt: str
+    repetition_penalty: float = 1.
     temperature: float = 0.1
     top_k: int
     top_p: float
